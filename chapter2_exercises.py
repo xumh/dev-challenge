26545e7317cafcc11d8f14e4dc4991c2a77c9a00
Mengtan <mengtan@startupinstitute.com>
2013-12-30 18:13:31 -0500

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,36 @@
 # Exercises for chapter 2:
-
-# Name:
+# 2.1
+01 #1
+010 #8
+0100 #64
+01000 #512
+# 2.2
+print 5
+x = 5
+print x + 1
+# 2.3
+width = 17
+height = 12.0
+delimiter = "."
+# 2.3.1: 8
+print width/2.0
+# 2.3.2: 8.5
+print height/3
+# 2.3.3: 4.0
+print 1+2*5
+# 2.3.4: 11
+print delimiter *5
+# 2.3.5: .....
+# 2.4.1: 523.60
+import math
+print 4/3 * math.pi * 5**3
+# 2.4.2: 945.45
+print 59*0.75+3+24.95*0.6*60
+# 2.4.3: 7:30
+start = (6*60+52)*60
+easy = (8*60+15)*2
+fast = (7*60+12)*3
+finish_hour = (start + easy + fast)/(60*60.0)
+finish_floored = (start + easy + fast)/(60*60)
+finish_minute  = (finish_hour - finish_floored)*60
+print finish_floored,":",finish_minute
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,43 @@
 # Exercises for chapter 3:
+# Name: Mengtan
+# 3.1, 3.2 NameError: name 'repeat_lyrics' is not defined
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+repeat_lyrics()
+# 3.3
+def right_justify(s):
+    print " "*(70-len(s))+s
+    
+right_justify('allen')
+# 3.4.1-4
+def do_twice(f, value):
+    f(value)
+    f(value)
 
-# Name:
+def print_twice(anything):
+    print anything
+    print anything
+do_twice(print_twice, 'spam')
+# 3.4.5
+def do_four(f, value):
+    do_twice(f, value)
+    do_twice(f, value)
+    
+do_four(print_twice, 'spa')
+# 3.5
+def grid():
+	sp = '|', " "*4,'|', " "*4,'|'
+	jia = "+", "-"*4,"+", "-"*4,"+"
+	print jia
+	print_twice(sp)
+	print_twice(sp)
+	print jia
+	print_twice(sp)
+	print_twice(sp)
+	print jia
+    
+grid()	
